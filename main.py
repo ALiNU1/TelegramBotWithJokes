205d672a10e5097059c0899667f85793069ee2bb
alinu1 <uwu1343@gmail.com>
2023-02-11 10:01:13 +0600

    finish

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import random
+import telebot
+from bs4 import BeautifulSoup as b
+
+URL = 'https://www.anekdot.ru/last/good'
+API_KEY = '6006830318:AAE3nnOPBD8xhw6fkDi5XjB3L7iMhYhfLhw'
+def parser(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    anekdots = soup.find_all('div', class_='text')
+    return [c.text for c in anekdots]
+
+list_of_jokes = parser(URL)
+random.shuffle(list_of_jokes)
+
+bot  = telebot.TeleBot(API_KEY)
+@bot.message_handler(commands=['Начать'])
+
+def hello(message):
+    bot.send_message(message.chat.id, 'Привет! Чтобы посмеяться ввелите любую цифру')
+
+@bot.message_handler(content_types=['text'])
+def jokes(message):
+    if message.text.lower() in '123456789':
+        bot.send_message(message.chat.id, list_of_jokes[0])
+        del list_of_jokes[0]
+    else:
+        bot.send_message(message.chat.id, 'Ввелите любую цифру')
+
+bot.polling()
